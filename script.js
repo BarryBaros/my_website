66269937d1019ec6556b3228c407c4d5000637a0
Timothy Barasa <timbaros87@gmail.com>
2024-03-29 17:54:20 +0300

    add aler messages.

--- script.js
+++ script.js
@@ -1,4 +1,8 @@
 const h2 = document.createElement("h2");
-h2.textContent = "This content added by JavaScript";
-
+h2.textContent = "Welcome to my Website";
 document.querySelector("body").appendChild(h2);
+document.title = "My Website";
+
+alert("How can I help you?");
+confirm("Male or Female")
+prompt("Enter gender:")
